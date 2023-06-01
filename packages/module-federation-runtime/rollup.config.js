3e7c63b5e999e86052869fec1e5552a387081ed6
Jiamin Li <jiamin.li@moodys.com>
2023-06-01 17:39:51 -0400

    add support for commonjs

--- packages/module-federation-runtime/rollup.config.js
+++ packages/module-federation-runtime/rollup.config.js
@@ -1,19 +1,26 @@
-import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
-import serve from 'rollup-plugin-serve';
+import babel from "rollup-plugin-babel"
+import resolve from "rollup-plugin-node-resolve"
+import commonjs from "rollup-plugin-commonjs"
+import { terser } from "rollup-plugin-terser"
+import serve from "rollup-plugin-serve"
 
 export default {
   input: "src/index.js",
-  output: {
-    file: 'dist/index.js',
-    format: "es",
-    sourcemap: true,
-  },
+  output: [
+    {
+      file: "dist/index.cjs.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "es",
+      sourcemap: true,
+    },
+  ],
   plugins: [
     babel({
-      exclude: 'node_modules/**',
+      exclude: "node_modules/**",
     }),
     // serve({
     //   open: true,
