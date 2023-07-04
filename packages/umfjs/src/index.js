f55fd95a0e7ab4f0b80f052d74637f51e57e8af5
zhangHongEn <zhang_h_n@163.com>
2023-07-04 11:54:14 +0800

    safari fix

--- packages/umfjs/src/index.js
+++ packages/umfjs/src/index.js
@@ -1,9 +1,7 @@
-const { findShare } = require("./utils/findShare")
 // const interceptSystemDep = require("./utils/interceptSystemDep")
 const interceptSystemAllDep = require("./utils/interceptSystemAllDep")
 
 export {
-  findShare,
   // interceptSystemDep,
   interceptSystemAllDep
 }
