adc6840f349a4a611d22688ce426354022a1f5e9
zhangHongEn <zhang_h_n@163.com>
2023-09-05 16:21:11 +0800

    Compatible with lower versions

--- packages/umfjs/src/index.js
+++ packages/umfjs/src/index.js
@@ -1,7 +1,9 @@
 // const interceptSystemDep = require("./utils/interceptSystemDep")
+const { findShare } = require("./utils/findShare")
 const interceptSystemAllDep = require("./utils/interceptSystemAllDep")
 
 export {
+  findShare,
   // interceptSystemDep,
   interceptSystemAllDep
 }
---
+++ packages/umfjs/src/utils/findShare.js
@@ -0,0 +1,24 @@
+const {rangesMaxSatisfying} = require("./semver")
+
+module.exports.findShare = function getShare(pkg, shareConfig = {}, shareScopes) {
+  // TODO: shareConfig fill default
+  if (!shareScopes) throw new Error("shareScopes is required")
+  const {strictVersion, singleton, shareScope = "default", requiredVersion = "*"} = shareConfig
+  if (!shareScopes[shareScope] || !shareScopes[shareScope][pkg]) {
+    return null
+  }
+  const pkgVersions = shareScopes[shareScope][pkg]
+  const loadedShareVersions = Object.keys(pkgVersions).filter(version => !!pkgVersions[version].loaded)
+  const rangeMax = rangesMaxSatisfying(Object.keys(pkgVersions), requiredVersion)
+  const max = rangesMaxSatisfying(Object.keys(pkgVersions), "*")
+  const loadedRangeMax = rangesMaxSatisfying(loadedShareVersions, requiredVersion)
+  const loadedMax = rangesMaxSatisfying(loadedShareVersions, "*")
+  const useShareVersion = singleton ? 
+  loadedRangeMax || loadedMax || rangeMax || max :
+    rangeMax || max
+  if (strictVersion && !rangesMaxSatisfying([useShareVersion], requiredVersion)) {
+    throw new Error(`Unsatisfied version \${useShareVersion} from app1 of shared singleton module react (required ${requiredVersion})
+    at getStrictSingletonVersion `)
+  }
+  return [useShareVersion, shareScopes[shareScope][pkg][useShareVersion]]
+}
